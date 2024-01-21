234df5476c0ea2a83733d938a1b486d28f00187a
RandomThacker <randomthacker@gmail.com>
2024-01-21 13:56:11 +0530

    fixed hero section + vide + carousel section

--- 3rdSection/script.js
+++ 3rdSection/script.js
@@ -1,4 +1,3 @@
-// import Splide from "@splidejs/splide";
 
 var splide3 = new Splide("#splide", {
   classes: {
@@ -7,49 +6,37 @@ var splide3 = new Splide("#splide", {
   },
   autoplay: "play",
   pauseOnHover: false,
-
-  // intersection: {
-  //   inView: {
-  //     autoplay: true,
-  //   },
-  //   outView: {
-  //     autoplay: false,
-  //   },
-  // },
   type: "loop",
   perPage: 1,
   arrows: false,
   interval: 2500,
-  // speed: 40,
 });
 
 splide3.mount(window.splide3.Extensions);
 
-// gsap styles, maybe not required idk
+const splitTypes = document.querySelectorAll(".reveal-type");
 
-// const splitTypes = document.querySelectorAll(".reveal-type");
+splitTypes.forEach((char, i) => {
+  const text = new SplitType(char, { types: "chars" });
 
-// splitTypes.forEach((char, i) => {
-//   const text = new SplitType(char, { types: "chars" });
+  // Set initial opacity to 0
+  gsap.set(text.chars, { opacity: 1 });
 
-//   // Set initial opacity to 0
-//   gsap.set(text.chars, { opacity: 1 });
-
-//   gsap.from(
-//     text.chars,
-//     {
-//       scrollTrigger: {
-//         trigger: "#main",
-//         start: "11.5% top",
-//         end: "20% top",
-//         scrub: true,
-//         pin: true,
-//         // markers: true,
-//       },
-//       duration: 0.5, // Adjust the duration as needed
-//       stagger: 0.1,
-//       opacity: 0.2, // Set the target opacity to 1
-//     },
-//     "a"
-//   );
-// });
+  gsap.from(
+    text.chars,
+    {
+      scrollTrigger: {
+        trigger: ".splide",
+        start: "top top",
+        end: "center top",
+        scrub: true,
+        // pin: true,
+        markers: true,
+      },
+      duration: 0.5, // Adjust the duration as needed
+      stagger: 0.1,
+      opacity: 0.2, // Set the target opacity to 1
+    },
+    "a"
+  );
+});
--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const lenis = new Lenis({
   duration: 4,
   smooth: true,
-  // easing: (t, friction = 0.1) => {
-  //   // Apply friction to the easing function
-  //   const easedValue = Math.min(1, 1.001 - Math.pow(2, -10 * t));
-
-  //   // Apply friction to the eased value
-  //   // const frictionedValue = easedValue - friction * t;
-
-  //   return Math.min(1, frictionedValue);
-  // },
 });
 function raf(time) {
   lenis.raf(time);
@@ -25,23 +16,24 @@ document.body.addEventListener("mousemove", function (dets) {
   });
 });
 
-// function showNav() {
-//   document.getElementsByClassName("navigation")[0].classList.toggle("active");
-//   // Get the body element
-//   var body = document.body;
+var splide3 = new Splide("#splide", {
+  classes: {
+    pagination: "splide__pagination tyre-pagination",
+    page: "splide__pagination__page your-class-page",
+  },
+  autoplay: "play",
+  pauseOnHover: false,
+  type: "loop",
+  perPage: 1,
+  arrows: false,
+  interval: 2500,
+});
 
-//   // Toggle the 'no-scroll' class on the body
-//   body.classList.toggle("no-scroll");
-// }
+splide3.mount(window.splide3.Extensions);
 
 function disableScroll() {
-  // Get the current scroll position
   var scrollY = window.scrollY;
-
-  // Save the current scroll position to scroll back to it later
   document.body.dataset.scrollY = scrollY;
-
-  // Disable scroll by setting the body's position to fixed and top to negative of the scroll position
   document.body.style.position = "fixed";
   document.body.style.top = `-${scrollY}px`;
 }
@@ -85,7 +77,6 @@ document.addEventListener("DOMContentLoaded", function () {
     height: "50vh",
     wheel: true,
     perPage: 6,
-    // type: "loop",
     perMove: 1,
     pagination: false,
     gap: 15,
@@ -93,13 +84,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
-
-// gsap.from(".heroCanvas", {
-//   x: 1300,
-//   rotate: 360,
-//   // repeat:-1,
-//   duration: 4,
-// });
 
 function dom() {
   const canvas = document.querySelector(".heroSection>canvas");
@@ -145,7 +129,7 @@ function dom() {
       trigger: "#main",
       start: "top top",
       end: "3% top",
-      // markers:true,
+      // markers: true,
     },
     onUpdate: render,
   });
@@ -184,57 +168,16 @@ function dom() {
       trigger: ".navBar",
       start: "top top",
       end: "center top",
-      // markers:true,
+      // markers: true,
     },
   });
-  // gsap.to(".heroSection>canvas", {
-  //   scale: 0.8,
-  //   scrollTrigger: {
-  //     scrub: 0.1,
-  //     trigger: "#main",
-  //     start: "bottom 100%",
-  //     invalidateOnRefresh: true,
-  //   },
-  // });
-
-  // ScrollTrigger.create({
-  //   trigger: "#main",
-  //   pin: true,
-  //   start: "bottom 100%",
-  // });
 }
 
 dom();
 
+
 function videoSection() {
-  let video = document.querySelector(".videoSection video");
-
-  ScrollTrigger.create({
-    trigger: "#main",
-    markers: true,
-    start: "3% top",
-    end: "30% top",
-    // pin:".videoSection",
-    onEnter: () => video.play(),
-    onEnterBack: () => video.play(),
-    onLeave: () => video.pause(),
-    onLeaveBack: () => video.pause(),
-  });
-
-  // let tl1 = gsap.timeline({
-  //   scrollTrigger: {
-  //     scrub: 0.1,
-  //     trigger: "#main",
-  //     start: "11.5% top",
-  //     end: "40% top",
-  //     pin: true,
-  //     markers:true,
-  //   },
-  // });
-
-  // -------------
   const splitTypes = document.querySelectorAll(".reveal-type");
-
   splitTypes.forEach((char, i) => {
     const text = new SplitType(char, { types: "chars" });
 
@@ -246,188 +189,98 @@ function videoSection() {
       {
         scrollTrigger: {
           trigger: "#main",
-          start: "11.5% top",
+          start: "14.3% top",
           end: "20% top",
           scrub: true,
-          pin: true,
-          // markers: true,
+          // pin: true,
+          markers: true,
         },
         duration: 0.5, // Adjust the duration as needed
         stagger: 0.1,
         opacity: 0.2, // Set the target opacity to 1
       },
-      "a"
     );
   });
-  // ------------
-
-  if (window.innerWidth < 600) {
-    // tl1.to(".videoOverlay", { opacity: 0.85 }, "+.4");
-    // tl1.to(".videoOverlayText", { bottom: "40%", duration: 8 }, "+.8");
-  } else {
-    // tl1.to(".videoOverlay", { opacity: 0.85 }, "+.4");
-    // tl1.to(".videoOverlayText", { bottom: "30%", duration: 8 }, "+.8");
-  }
+  ScrollTrigger.create({
+    trigger: "#main",
+    markers: true,
+    start: "14.3% top",
+    end: "20% top",
+    pin: true
+    // pin:".videoSection",
+  });
 }
+
 videoSection();
 
-function horizontalScroll() {
-  let images = gsap.utils.toArray(".horizontal__item");
-  let animationProps = {
-    ease: "none",
-    scrollTrigger: {
-      trigger: "#main",
-      pin: true,
-      start: "23% top",
-      end: "50% top",
-      scrub: 1,
-      // markers: true,
-    },
-    defaults: { duration: 1 },
-  };
-  // gsap.to(images, {
-  //   xPercent: -50 * (images.length - 1),
-  //   ...animationProps,
-  //   onComplete: function () {
-  //     gsap.delayedCall(10, () => {
-  //       // Your code here that you want to execute after a 1-second delay at the end of the animation
-  //       console.log("Animation completed. Adding a 1-second delay at the end.");
-  //     });
-  //   },
-  // });
-
-  // gsap.to(images, {
-  //   xPercent: -50 * (images.length - 1),
-  //   ...animationProps,
-  // });
-
-  // Check the screen width
-  if (window.innerWidth < 600) {
-    gsap.to(images, {
-      xPercent: -160 * (images.length - 1),
-      ...animationProps,
-    });
-  } else if (window.innerWidth >= 1000 && window.innerWidth < 1200) {
-    gsap.to(images, {
-      xPercent: -45 * (images.length - 1),
-      ...animationProps,
-    });
-  } else if (window.innerWidth >= 700 && window.innerWidth < 900) {
-    gsap.to(images, {
-      xPercent: -60 * (images.length - 1),
-      ...animationProps,
-    });
-  } else {
-    gsap.to(images, {
-      xPercent: -50 * (images.length - 1),
-      ...animationProps,
-    });
-  }
-}
-
-horizontalScroll();
-
-function curvedScroll() {
-  gsap.registerPlugin(ScrollTrigger);
-
-  const slides = gsap.utils.toArray(".single-item");
-
-  var yPercentValue;
-  if (window.innerWidth < 600) {
-    // Adjust for small screens
-    yPercentValue = 0;
-  } else {
-    // Default value for larger screens
-    yPercentValue = -4;
-  }
-
-  const animation = () => {
-    gsap.set(".carrousel-wrappper", {
-      xPercent: -50,
-      yPercent: yPercentValue,
-      width: `${slides.length * 350}px`,
-      height: `${slides.length * 350}px`,
-    });
-    circleSetup(".carrousel-wrappper", ".single-item", -50);
-  };
-
-  gsap.to(".curvedScroll", {
-    scrollTrigger: {
-      trigger: "#main",
-      pin: true,
-      start: "34.49% top",
-      end: "55% top",
-      // markers: true,
-      onUpdate: (self) => {
-        gsap.set(".carrousel-wrappper", { rotation: self.progress * 200 });
+function splitCarousel(){
+  
+  const splitTypes = document.querySelectorAll(".reveal-type1");
+  splitTypes.forEach((char, i) => {
+    const text = new SplitType(char, { types: "chars" });
+  
+    // Set initial opacity to 0
+    gsap.set(text.chars, { opacity: 1 });
+  
+    gsap.from(
+      text.chars,
+      {
+        scrollTrigger: {
+          trigger: "#main",
+          start: "28.5% top",
+          end: "34% top",
+          scrub: true,
+          // pin: true,
+          markers: true,
+        },
+        duration: 0.5, // Adjust the duration as needed
+        stagger: 0.1,
+        opacity: 0.2, // Set the target opacity to 1
       },
-    },
+    );
   });
-
-  /// set slides around the circle
-  const circleSetup = (circle, items, percentageValue) => {
-    const mainCicle = document.querySelector(circle);
-    const circleItem = gsap.utils.toArray(items);
-
-    const radius = mainCicle.offsetWidth / 2;
-    const center = mainCicle.offsetWidth / 2;
-    const total = circleItem.length;
-    const slice = (-1.3 * Math.PI) / total;
-
-    circleItem.forEach((item, i) => {
-      const angle = i * slice;
-
-      const x = center + radius * Math.sin(angle);
-      const y = center - radius * Math.cos(angle);
-
-      gsap.set(item, {
-        rotation: angle + "_rad",
-        xPercent: percentageValue,
-        yPercent: percentageValue,
-        x,
-        y,
-      });
-    });
-  };
-
-  window.addEventListener("load", () => {
-    animation();
-    gsap.set("main", { autoAlpha: 1 });
+  ScrollTrigger.create({
+    trigger: "#main",
+    markers: true,
+    start: "28.5% top",
+    end: "34% top",
+    pin: true
+    // pin:".videoSection",
   });
-}
-curvedScroll();
+  }
+  splitCarousel()
 
 function bullStory() {
   ScrollTrigger.create({
     trigger: "#main",
-    // markers: true,
-    start: "46% top",
-    end: "53% top",
+    markers: true,
+    start: "43.5% top",
+    end: "55% top",
     pin: true,
   });
-  if (window.innerWidth < 600) {
-    gsap.to(".bullStoryOverlay", {
-      scrollTrigger: {
-        trigger: "#main",
-        // markers: true,
-        start: "46% top",
-        end: "52% top",
-        scrub: 2,
-      },
-      top: "50%",
-    });
-  } else {
-    gsap.to(".bullStoryOverlay", {
-      scrollTrigger: {
-        trigger: "#main",
-        // markers: true,
-        start: "46% top",
-        end: "52% top",
-        scrub: 2,
-      },
-      top: "55%",
-    });
-  }
+  // if (window.innerWidth < 600) {
+  //   gsap.to(".bullStoryOverlay", {
+  //     scrollTrigger: {
+  //       trigger: "#main",
+  //       markers: true,
+  //       start: "48.5% top",
+  //       end: "55% top",
+  //       scrub: 2,
+  //     },
+  //     top: "50%",
+  //   });
+  // } else {
+  //   gsap.to(".bullStoryOverlay", {
+  //     scrollTrigger: {
+  //       trigger: "#main",
+  //         markers: true,
+  //       start: "48.5% top",
+  //       end: "52% top",
+  //       scrub: 2,
+  //     },
+  //     top: "55%",
+  //   });
+  // }
 }
 bullStory();
 
@@ -575,3 +428,9 @@ function ecosystem() {
 }
 
 ecosystem();
+
+// ----------------Splide section----------------------
+
+
+
+
